5a1eb7e9c39aa155994fb6a6f8cef5df2327d031
Nate <jamesonn@uwm.edu>
2017-11-05 16:50:34 -0600

    sidebar cleanup

--- MKESocial/app/src/main/java/team2/mkesocial/Activities/FeedActivity.java
+++ MKESocial/app/src/main/java/team2/mkesocial/Activities/FeedActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v4.app.FragmentActivity;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;

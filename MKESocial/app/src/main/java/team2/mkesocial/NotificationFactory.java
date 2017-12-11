a1a5cb75963603487a01a03f6f2a39d2a55adf8f
Nate <jamesonn@uwm.edu>
2017-12-10 23:32:30 -0600

    notification architecture

--- MKESocial/app/src/main/java/team2/mkesocial/NotificationFactory.java
+++ MKESocial/app/src/main/java/team2/mkesocial/NotificationFactory.java
@@ -1,16 +1,35 @@
 package team2.mkesocial;
 
-import android.content.BroadcastReceiver;
-import android.content.Context;
+import android.app.IntentService;
+import android.app.Notification;
+import android.app.PendingIntent;
 import android.content.Intent;
+import android.support.v4.app.NotificationManagerCompat;
+
+import team2.mkesocial.Activities.FeedActivity;
 
 /**
  * Created by Nate on 12/10/2017.
  */
 
-public class NotificationFactory extends BroadcastReceiver{
-    @Override
-    public void onReceive(Context context, Intent intent){
+public class NotificationFactory extends IntentService{
 
+    private static final int NOTIFY_ID = 321;
+
+    public NotificationFactory() {
+        super("NotificationFactory");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        Notification.Builder builder = new Notification.Builder(this);
+        builder.setContentTitle("My Title");
+        builder.setContentText("This is the Body");
+        Intent notifyIntent = new Intent(this, FeedActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 2, notifyIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        builder.setContentIntent(pendingIntent);
+        Notification notificationCompat = builder.build();
+        NotificationManagerCompat managerCompat = NotificationManagerCompat.from(this);
+        managerCompat.notify(NOTIFY_ID, notificationCompat);
     }
 }

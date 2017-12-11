689bc7e1ce8fab67b2d8d1a349d22a809eb163fd
Nate <jamesonn@uwm.edu>
2017-12-11 15:41:58 -0600

    status update

--- MKESocial/app/src/main/java/team2/mkesocial/NotificationFactory.java
+++ MKESocial/app/src/main/java/team2/mkesocial/NotificationFactory.java
@@ -3,8 +3,10 @@ package team2.mkesocial;
 import android.app.AlarmManager;
 import android.app.IntentService;
 import android.app.Notification;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationManagerCompat;
 
 import com.google.firebase.auth.FirebaseUser;
@@ -40,6 +42,12 @@ public class NotificationFactory extends IntentService implements ValueEventList
         Notification notificationCompat = builder.build();
         NotificationManagerCompat managerCompat = NotificationManagerCompat.from(this);
         managerCompat.notify(NOTIFY_ID, notificationCompat);
+
+
+        int mNotificationId = 001;
+        NotificationManager mNotifyMgr = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+        mNotifyMgr.notify(mNotificationId, notificationCompat);
     }
 
     @Override
@@ -52,15 +60,8 @@ public class NotificationFactory extends IntentService implements ValueEventList
             Query _dataQuery = df.getReference(Event.DB_EVENTS_NODE_NAME).orderByChild("title");
             _dataQuery.addValueEventListener(this);
         }
-        //Notification.Builder builder = new Notification.Builder(this);
-        //builder.setContentTitle("My Title");
-        //builder.setContentText("This is the Body");
-        //Intent notifyIntent = new Intent(this, FeedActivity.class);
-        //PendingIntent pendingIntent = PendingIntent.getActivity(this, 2, notifyIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-        //builder.setContentIntent(pendingIntent);
-        //Notification notificationCompat = builder.build();
-        //NotificationManagerCompat managerCompat = NotificationManagerCompat.from(this);
-        //managerCompat.notify(NOTIFY_ID, notificationCompat);
+
+
     }
 
     @Override
